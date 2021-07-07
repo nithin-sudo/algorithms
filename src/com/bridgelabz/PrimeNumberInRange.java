63e004025395516a645918c49720234983c4fca9
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-07 23:26:37 +0530

    Added: primeInRange method and printing primes between range.

---
+++ src/com/bridgelabz/PrimeNumberInRange.java
@@ -0,0 +1,36 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class PrimeNumberInRange {
+    /**
+     * printing the primes between given range.
+     * @param lower
+     * @param upper
+     */
+    public static void primeInRange (int lower,int upper){
+        System.out.println("Prime numbers between " + lower + " and " + upper + " are : ");
+        int count;
+        for (int i = lower; i <= upper; i++) {
+            count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0)
+                    count = count+1;
+            }
+            if (count == 2)
+                System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        PrimeNumberInRange primeNumberInRange = new PrimeNumberInRange();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the lower range : ");
+        int lower = scanner.nextInt();
+        System.out.print("Enter the upper range : ");
+        int upper = scanner.nextInt();
+        scanner.close();
+        primeNumberInRange.primeInRange(lower,upper);
+    }
+
+}
